ead991aac2165978a43f4501ab54f35b8eb08bc7
Modbilas <238.juanjen@gmail.com>
2023-10-09 15:51:20 +0700

    navbar
    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { Container, Typography, Button, Grid, Card, CardContent } from '@mui/material';
+import { Container, Typography, Button, Grid, Card, Box , IconButton, CardContent, AppBar, Toolbar } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const PetFeederPage = () => {
 
 
   return (
     <Container maxWidth="md">
-      <Typography variant="h4" align="center" gutterBottom>
-        Automatic Pet Feeder
-      </Typography>
+      <Box sx={{ flexGrow: 1, mb:2 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" /*sx={{ flexGrow: 1 }}*/>
+              Home
+            </Typography>
+            <Typography variant="h5" component="div" align='center' sx={{ flexGrow: 1 }}>
+              Automatic Pet Feeder
+            </Typography>
+            <Button color="inherit">Login</Button>
+            </Toolbar>
+        </AppBar>
+      </Box>
       <Grid container spacing={2}>
         <Grid item xs={12} sm={6}>
           <Card>
@@ -16,7 +36,6 @@ const PetFeederPage = () => {
               <Typography variant="h6" gutterBottom>
                 Feeding Schedule
               </Typography>
-
             </CardContent>
           </Card>
         </Grid>
@@ -30,7 +49,7 @@ const PetFeederPage = () => {
           </Card>
         </Grid>
       </Grid>
-      <Button variant="contained" color="primary" fullWidth>
+      <Button variant="contained" color="primary" fullWidth sx={{ flexGrow: 1, mt:2 }}>
         Feed My Pet
       </Button>
     </Container>
